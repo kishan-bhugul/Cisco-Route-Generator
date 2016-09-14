c4543fbc0a17c8410ae3172c8bf5d2787c6cdcd2
Kishan Bhugul <kishan-bhugul@users.noreply.github.com>
2016-09-14 15:22:07 +0400

    Create Cisco-Route-Generator.py

---
+++ Cisco-Route-Generator.py
@@ -0,0 +1,16 @@
+#Cisco Static Route Generator
+#Autohor Kishan Bhugul
+#Version 1.0
+
+file = open('ipRoutes.txt', 'r')
+
+for line in file:
+        count=0
+        for word in line.split():
+            if(count==0):
+               x = "ip route "+word+" 255.255.255.255 "
+               count=count+1
+            else:
+                x=x+word
+
+                print x
